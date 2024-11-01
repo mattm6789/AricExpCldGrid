b90d87b24eb11dcf09ab85994461342d1a7a2032
MattM6789 <mattm6789@gmail.com>
2024-11-01 14:53:05 -0500

    still trying to access the image

--- AricDevOrg/force-app/main/default/lwc/imageGrid/imageGrid.js
+++ AricDevOrg/force-app/main/default/lwc/imageGrid/imageGrid.js
@@ -1,3 +1,6 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from 'lwc';
 
-export default class ImageGrid extends LightningElement {}
+export default class ImageGrid extends LightningElement {
+    @api url = 'https://aricexpcldgrid-dev-ed.trailblaze.my.site.com/AuraTemplate/s/page-1';
+    @api imageSrc = 'Designer_1.jpeg';
+}
