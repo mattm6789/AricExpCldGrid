536117bf3661f7add51ac6c01e78329eae38ad21
MattM6789 <mattm6789@gmail.com>
2024-11-02 09:25:54 -0500

    24-1102 0925

--- AricDevOrg/force-app/main/default/lwc/imageGrid/imageGrid.js
+++ AricDevOrg/force-app/main/default/lwc/imageGrid/imageGrid.js
@@ -1,6 +1,8 @@
 import { LightningElement, api } from 'lwc';
-
+import ROBOTS_ON_BEACH from "@salesforce/contentAssetUrl/Designer_1";
 export default class ImageGrid extends LightningElement {
+
+
     @api url = 'https://aricexpcldgrid-dev-ed.trailblaze.my.site.com/AuraTemplate/s/page-1';
-    @api imageSrc = 'Designer_1.jpeg';
+    @api imageSrc = ROBOTS_ON_BEACH;
 }
